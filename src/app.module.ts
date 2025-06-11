643e8e8ac865042707897b535696753d153b987f
Loop1397 <shwlsdn0906@gmail.com>
2025-06-11 18:49:35 +0900

    feat: TypeORM 설정
    mysql을 사용하기 위한 TypeORM 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GameModule } from './game/game.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    GameModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
