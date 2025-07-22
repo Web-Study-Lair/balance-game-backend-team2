c8a959557a37b586f7f43f24663bb528c918bcca
Loop1397 <shwlsdn0906@gmail.com>
2025-07-23 02:09:24 +0900

    fix: get의 링크가 겹쳐 findGamesByUserID가 제대로 동작하지 않았던 문제 해결
    패스 파라미터가 같은 Get이 두개 있었기에 findGamesByUserID의 controller에 user/패스 파라미터를 추가함

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Query } from '@nestjs/common';
 import { GameService } from './game.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { DeleteGameDto } from './dto/delete-game.dto';
@@ -7,22 +7,22 @@ import { DeleteGameDto } from './dto/delete-game.dto';
 export class GameController {
   constructor(private readonly gameService: GameService) { }
 
-  @Post('/')
+  @Post()
   async createGame(@Body() createGameDto: CreateGameDto) {
     return await this.gameService.createGame(createGameDto);
   }
 
-  @Get('/')
+  @Get()
   async findAllGames() {
     return await this.gameService.findAllGames();
   }
 
-  @Get('/:userId')
-  findOneGame(@Param('userId') userId: number) {
-    return this.gameService.findGamesByUserID(userId);
+  @Get('user')
+  async findGamesByUserID(@Query('userId') userId: number) {
+    return await this.gameService.findGamesByUserID(userId);
   }
 
-  @Delete('/')
+  @Delete()
   async removeGame(@Body() deleteGameDto: DeleteGameDto) {
     return await this.gameService.removeGame(deleteGameDto);
   }
